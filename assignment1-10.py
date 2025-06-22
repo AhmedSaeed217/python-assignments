8f31b3b4c80e215f006fc236d94c3d60859155c3
AhmedSaeed217 <ahmedsaeedfci@gmail.com>
2025-06-22 23:18:53 +0300

    adding Assignment file from lesson 1 To 10

---
+++ assignment1-10.py
@@ -0,0 +1,40 @@
+# 1-Create a new file to start writing the code in, then at the top of the file write a multi-line comment describing the file.
+
+#---------------------------------
+# This is the first assignment 
+# Multiple line comments 
+#---------------------------------
+
+#************************************************************#
+
+
+# 2-Create three variables containing your name, age, and country, of type String.
+ 
+myName='Ahmed'
+myAge="25"
+myCountry="Egypt"
+
+#************************************************************#
+
+
+# 3-Print the values ​​in the previous variables on three lines, one below the other, and before them a name that represents the variables.
+
+print("Name : "+myName)
+print("Age : "+myAge)
+print("Country : "+myCountry)
+
+#************************************************************#
+
+# 4-Concatenate the variables with some words to produce this message in the following output: "Hello, My Name Is Osama And Iam 38 Years Old and I Live in Egypt."
+print("Hello, My Name Is " + myName +" And Iam " + myAge + " Years Old and I Live in " +myCountry +".")
+
+#************************************************************#
+
+# 5-Print the type of each variable we created on a separate line.
+
+print(type(myName))
+print(type(myAge))
+print(type(myCountry))
+
+
+#************************************************************#
